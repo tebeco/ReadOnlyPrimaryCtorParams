a9b0b9076c3722e43cc044aab8e646aeb36e5db6
TeBeCo <tebeco@gmail.com>
2025-04-16 01:06:15 +0400

    🦤🦤🦤🦤

--- ReadOnlyPrimaryCtorParams.Sample/SampleTypes.cs
+++ ReadOnlyPrimaryCtorParams.Sample/SampleTypes.cs
@@ -24,88 +24,88 @@ public partial class NestedParentLevel0
     }
 }
 
-//public partial class NestedParent<T>
-//{
-//    public partial class NestedChild([ReadOnly] Bar bar)
-//    {
-//        public void Get()
-//        {
-//            Console.WriteLine(bar.Name);
-//            bar = null!;
-//        }
-//    }
-//}
+public partial class NestedParent<T>
+{
+    public partial class NestedChild([ReadOnly] Bar bar)
+    {
+        public void Get()
+        {
+            Console.WriteLine(bar.Name);
+            bar = null!;
+        }
+    }
+}
 
 
-//public partial class FooClass([ReadOnly] Bar bar)
-//{
-//    public void Get()
-//    {
-//        Console.WriteLine(bar.Name);
-//    }
-//}
+public partial class FooClass([ReadOnly] Bar bar)
+{
+    public void Get()
+    {
+        Console.WriteLine(bar.Name);
+    }
+}
 
-//public partial class FooClassMultipleParameter([ReadOnly] Bar barOne, [ReadOnly] Bar barTwo)
-//{
-//    public void Get()
-//    {
-//        Console.WriteLine(barOne.Name);
-//        Console.WriteLine(barTwo.Name);
-//    }
-//}
+public partial class FooClassMultipleParameter([ReadOnly] Bar barOne, [ReadOnly] Bar barTwo)
+{
+    public void Get()
+    {
+        Console.WriteLine(barOne.Name);
+        Console.WriteLine(barTwo.Name);
+    }
+}
 
-//public partial struct FooStruct([ReadOnly] Bar bar)
-//{
-//    public void Get()
-//    {
-//        Console.WriteLine(bar.Name);
-//    }
-//}
+public partial struct FooStruct([ReadOnly] Bar bar)
+{
+    public void Get()
+    {
+        Console.WriteLine(bar.Name);
+    }
+}
 
-//public partial record FooRecord([ReadOnly] Bar bar)
-//{
-//    public void Get()
-//    {
-//        Console.WriteLine(bar.Name);
-//    }
-//}
+public partial record FooRecord([ReadOnly] Bar bar)
+{
+    public void Get()
+    {
+        Console.WriteLine(bar.Name);
+    }
+}
 
-//public partial record struct FooRecordStruct([ReadOnly] Bar bar)
-//{
-//    public void Get()
-//    {
-//        Console.WriteLine(bar.Name);
-//    }
-//}
+public partial record struct FooRecordStruct([ReadOnly] Bar bar)
+{
+    public void Get()
+    {
+        Console.WriteLine(bar.Name);
+    }
+}
 
-//public partial class FooClassGeneric<T>([ReadOnly] Bar bar)
-//{
-//    public void Get()
-//    {
-//        Console.WriteLine(bar.Name);
-//    }
-//}
+public partial class FooClassGeneric<T>([ReadOnly] Bar bar)
+{
+    public void Get()
+    {
+        Console.WriteLine(bar.Name);
+    }
+}
 
-//public partial struct FooStructGeneric<T>([ReadOnly] Bar bar)
-//{
-//    public void Get()
-//    {
-//        Console.WriteLine(bar.Name);
-//    }
-//}
+public partial struct FooStructGeneric<T>([ReadOnly] Bar bar)
+{
+    public void Get()
+    {
+        Console.WriteLine(bar.Name);
+    }
+}
 
-//public partial record FooRecordGeneric<T>([ReadOnly] Bar bar)
-//{
-//    public void Get()
-//    {
-//        Console.WriteLine(bar.Name);
-//    }
-//}
+public partial record FooRecordGeneric<T>([ReadOnly] Bar bar)
+{
+    public void Get()
+    {
+        Console.WriteLine(bar.Name);
+    }
+}
 
-//public partial record struct FooRecordStructGeneric<T1, T2, T3>([ReadOnly] Bar bar)
-//{
-//    public void Get()
-//    {
-//        Console.WriteLine(bar.Name);
-//    }
-//}
+public partial record struct FooRecordStructGeneric<T1, T2, T3>([ReadOnly] Bar bar)
+{
+    public void Get()
+    {
+        Console.WriteLine(bar.Name);
+    }
+}
